a97970067e26b45717433f0dd99c4c1b9fe25463
segundocarlos <segundocarlos@hotmail.com>
2018-11-01 16:34:04 -0500

    LoginPage Enlazada con  el LoginViewModel activando el control switch

---
+++ Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,57 @@
+﻿
+namespace Lands.ViewModels
+{
+
+    using System.Windows.Input;
+
+    public class LoginViewModel
+    {
+        #region Properties
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        public bool IsRunning
+        {
+            get;
+            set;
+        }
+
+        public bool IsRemembered
+        {
+            get;
+            set;
+        }
+
+
+        #endregion
+
+        #region Command
+
+        public ICommand LoginCommand
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+
+        #region Constructors
+
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+        }
+
+        #endregion
+    }
+}
--- Lands/ViewModels/MainViewModel.cs
+++ Lands/ViewModels/MainViewModel.cs
@@ -3,6 +3,25 @@ namespace Lands.ViewModels
 {
     public class MainViewModel
     {
-       
+
+        #region ViewModels
+
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+
+        }
+
+        #endregion
+
     }
 }
