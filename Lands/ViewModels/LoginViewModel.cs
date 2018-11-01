013eba5dac435dce155558269b4fe6c6166e5e89
segundocarlos <segundocarlos@hotmail.com>
2018-11-01 18:19:45 -0500

    Se agrego libreria MvvmLightLibs para el control de enventos de botones Se agregaron los primeros alert..

--- Lands/ViewModels/LoginViewModel.cs
+++ Lands/ViewModels/LoginViewModel.cs
@@ -1,8 +1,11 @@
 ﻿
+
 namespace Lands.ViewModels
 {
 
     using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
+    using Xamarin.Forms;
 
     public class LoginViewModel
     {
@@ -38,9 +41,47 @@ namespace Lands.ViewModels
 
         public ICommand LoginCommand
         {
-            get;
-            set;
+            get 
+            {
+                return new RelayCommand(Login);
+
+            }
+
+
         }
+
+        private async void  Login()
+        {
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error",
+                                                          "You must enter an Email",
+                                                          "Accept");
+                return;
+            }
+            if (string.IsNullOrEmpty(this.Password))
+            {
+               await Application.Current.MainPage.DisplayAlert("Error",
+                                                          "You must enter a Password",
+                                                          "Accept");
+                return;
+            }
+
+            if (this.Email != "cf" || this.Password !="123")
+            {
+                await Application.Current.MainPage.DisplayAlert("Error",
+                                                           "Email or Password Incorrect",
+                                                           "Accept");
+                return;
+            }
+
+
+            await Application.Current.MainPage.DisplayAlert("OK",
+                                                           "Yeahhh",
+                                                           "Accept");
+
+        }
+
 
         #endregion
 
