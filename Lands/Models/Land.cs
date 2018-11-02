46c82555eca2c7f9708addb7c8973bda50a8139f
segundocarlos <segundocarlos@hotmail.com>
2018-11-02 16:27:03 -0500

    Se agrego el nuget Xam.PlugIn.ConnecctionActivity para controlar si hay actividad de internet en el dispositivo Se realizo la primer consumo del servicio rest de paises Se cambio parametro de HttpClient del file info.plist  del directorio lands.ios/resources a defaul para que permita conexiones no seguras ya que el servicio  de paises no es seguro.

--- Lands/Models/Land.cs
+++ Lands/Models/Land.cs
@@ -37,16 +37,16 @@ namespace Lands.Models
         public int Population { get; set; }
 
         [JsonProperty(PropertyName = "latlng")]
-        public List<int> Latlng { get; set; }
+        public List<double> Latlng { get; set; }
 
         [JsonProperty(PropertyName = "demonym")]
         public string Demonym { get; set; }
 
         [JsonProperty(PropertyName = "area")]
-        public int Area { get; set; }
+        public double? Area { get; set; }
 
         [JsonProperty(PropertyName = "gini")]
-        public double Gini { get; set; }
+        public double? Gini { get; set; }
 
         [JsonProperty(PropertyName = "timezones")]
         public List<string> Timezones { get; set; }
