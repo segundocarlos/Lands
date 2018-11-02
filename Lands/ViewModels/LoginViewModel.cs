eed9902e51626b02115c77c09d59bc8f5fff89b0
segundocarlos <segundocarlos@hotmail.com>
2018-11-01 19:05:48 -0500

    Implementacion de interfaz para poder borrar la caja de texto de password en caso de que haya sido ingresada una incorrecta

--- Lands/ViewModels/LoginViewModel.cs
+++ Lands/ViewModels/LoginViewModel.cs
@@ -2,13 +2,25 @@
 
 namespace Lands.ViewModels
 {
-
+    using System.ComponentModel;
     using System.Windows.Input;
     using GalaSoft.MvvmLight.Command;
     using Xamarin.Forms;
 
-    public class LoginViewModel
+    public class LoginViewModel : INotifyPropertyChanged
     {
+
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Atributos_a_refrescar_loginviewModel
+            private string email;
+            private string password;
+            private bool isrunning;
+            private bool isenabled;
+        #endregion
+
         #region Properties
         public string Email
         {
@@ -18,14 +30,29 @@ namespace Lands.ViewModels
 
         public string Password
         {
-            get;
-            set;
+            get { return this.password;  }
+            set {
+                if (this.password != value)
+                {
+                    this.password = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.Password)));
+                }
+
+            }
         }
 
         public bool IsRunning
         {
-            get;
-            set;
+            get { return this.isrunning; }
+            set
+            {
+                if (this.isrunning != value)
+                {
+                    this.isrunning = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.IsRunning)));
+                }
+
+            }
         }
 
         public bool IsRemembered
@@ -33,7 +60,19 @@ namespace Lands.ViewModels
             get;
             set;
         }
+        public bool IsEnabled
+        {
+            get { return this.isenabled; }
+            set
+            {
+                if (this.isenabled != value)
+                {
+                    this.isenabled = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(this.IsEnabled)));
+                }
 
+            }
+        }
 
         #endregion
 
@@ -41,7 +80,7 @@ namespace Lands.ViewModels
 
         public ICommand LoginCommand
         {
-            get 
+            get
             {
                 return new RelayCommand(Login);
 
@@ -50,7 +89,9 @@ namespace Lands.ViewModels
 
         }
 
-        private async void  Login()
+
+
+        private async void Login()
         {
             if (string.IsNullOrEmpty(this.Email))
             {
@@ -61,20 +102,37 @@ namespace Lands.ViewModels
             }
             if (string.IsNullOrEmpty(this.Password))
             {
-               await Application.Current.MainPage.DisplayAlert("Error",
-                                                          "You must enter a Password",
-                                                          "Accept");
-                return;
-            }
-
-            if (this.Email != "cf" || this.Password !="123")
-            {
                 await Application.Current.MainPage.DisplayAlert("Error",
-                                                           "Email or Password Incorrect",
+                                                           "You must enter a Password",
                                                            "Accept");
                 return;
             }
 
+
+            IsRunning = true;
+            IsEnabled = false;
+
+            if (this.Email != "cf" || this.Password != "123")
+            {
+
+                IsRunning = false;
+                IsEnabled = true;
+
+                await Application.Current.MainPage.DisplayAlert("Error",
+                                                           "Email or Password Incorrect",
+                                                           "Accept");
+
+                this.Password = string.Empty;
+                // se debe de implementar una interfaz INotifyPropertyChanged por que no se 
+                // blanquea la caja de texto password
+                // solo se blanquea la variable en el loginviewmodel, pero no esta blanqueado en la loginPage, por tal motivo la interfaz 
+                return;
+            }
+
+
+
+            IsRunning = false;
+            IsEnabled = true;
 
             await Application.Current.MainPage.DisplayAlert("OK",
                                                            "Yeahhh",
@@ -91,6 +149,7 @@ namespace Lands.ViewModels
         public LoginViewModel()
         {
             this.IsRemembered = true;
+            this.IsEnabled = true;
         }
 
         #endregion
