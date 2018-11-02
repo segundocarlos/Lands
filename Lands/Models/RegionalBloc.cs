8fc919f2011e8ef8c82d72a7c898787c1abb0908
segundocarlos <segundocarlos@hotmail.com>
2018-11-02 13:53:24 -0500

    Se agrega Nugets NewtonJson para poder dar otro nombre a las propiedades del servicio rest a consumir Tambien se crean los modelos que van a consumir los servicios

---
+++ Lands/Models/RegionalBloc.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Lands.Models
+{
+
+    using Newtonsoft.Json;
+
+    public class RegionalBloc
+    {
+        [JsonProperty(PropertyName = "acronym")]
+        public string Acronym { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+       
+    }
+}
