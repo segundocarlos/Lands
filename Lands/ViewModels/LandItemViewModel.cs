a521c8ced933ce4c86a8de1e300c4c415cb62cd7
segundocarlos <segundocarlos@hotmail.com>
2018-11-03 14:26:25 -0500

    Se realiza la navegacion a la pagina de detalle de cada pais

--- Lands/ViewModels/LandItemViewModel.cs
+++ Lands/ViewModels/LandItemViewModel.cs
@@ -1,11 +1,27 @@
 ﻿
 namespace Lands.ViewModels
 {
-
+    using System;
+    using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
     using Models;
+    using Xamarin.Forms;
+    using Views;
+
     public class LandItemViewModel: Land
     {
-       
+       public ICommand SelectLandCommand
+        {
+           
+            get {
+                return new RelayCommand(SelectLand);
+            }
+        }
 
+        private async void SelectLand()
+        {
+            MainViewModel.ObtenerInstancia().Land = new LandViewModel(this);
+            await Application.Current.MainPage.Navigation.PushAsync(new LandPage());
+        }
     }
 }
--- Lands/ViewModels/LandViewModel.cs
+++ Lands/ViewModels/LandViewModel.cs
@@ -1,9 +1,25 @@
 ﻿
 namespace Lands.ViewModels
 {
+    using Models;
     public class LandViewModel
     {
 
+        #region properties
+        public Land Land
+        {
+            get;
+            set;
+        }
 
+        #endregion
+
+
+        #region constructor
+        public LandViewModel(Land land)
+        {
+            this.Land = land;
+        }
+        #endregion
     }
 }
